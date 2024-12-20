fd619b3f2c3a62bb30e8f08be4018cf463e71b37
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2024-12-20 21:41:57 +0530

    navbar and home page is created

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
 
 //home route
 app.get("/home",(req,res)=>{
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./public/**/*.js"   
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pri-blue': '#004E98',
+        'background' : '#F8F8F8'
+      },
+    },
   },
   plugins: [],
 }
